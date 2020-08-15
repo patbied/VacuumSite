c5f10ccd2e7c2a9938737efd94caa249095ec3f8
Patrick Biedermann <patbied@gmail.com>
2020-08-15 21:53:55 +0200

    first commit/Quick contact fades out

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const menuBtn = document.querySelector('.menu-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileText = document.querySelectorAll('.mobileNavItem');
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
+  mobileNav.classList.toggle('open');
+  mobileText.forEach((link) => {
+    link.classList.toggle('open');
+  });
+});
+const quickContact = document.querySelector('#quickContactt');
+const hero = document.querySelector('.Hero');
+const header = document.querySelector('header');
+const navbar = document.querySelector('.navbar');
+const quickContactOptions = {
+  rootMargin: '-400px',
+};
+
+const quickContactObserver = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      quickContact.classList.add('scrolled');
+      header.classList.add('scrolled');
+      navbar.classList.add('scrolled');
+    } else {
+      quickContact.classList.remove('scrolled');
+      header.classList.remove('scrolled');
+      navbar.classList.remove('scrolled');
+    }
+  });
+}, quickContactOptions);
+
+quickContactObserver.observe(hero);
